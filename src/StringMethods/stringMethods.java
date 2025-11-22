88bfefb03d4493c1cdf43bb81ac3508a8f8ed518
elviragafarova <elvira.qafarova.rg.jv@idtech.edu.az>
2025-11-22 19:18:31 +0400

    Add StringMethods code

---
+++ src/StringMethods/stringMethods.java
@@ -0,0 +1,19 @@
+package StringMethods;
+public class stringMethods {
+        public static void main(String[] args) {
+
+            String text = "Qafarova Elvira";
+
+            System.out.println("length(): " + text.length());
+            System.out.println("charAt(2): " + text.charAt(2));
+            System.out.println("toUpperCase(): " + text.toUpperCase());
+            System.out.println("toLowerCase(): " + text.toLowerCase());
+            System.out.println("contains(): " + text.contains("Elvira"));
+            System.out.println("startsWith(): " + text.startsWith("Salam"));
+            System.out.println("endsWith(): " + text.endsWith("ra"));
+            System.out.println("indexOf(): " + text.indexOf("f"));
+            System.out.println("replace(): " + text.replace("Qafarova", "Eliyeva"));
+            System.out.println("substring(6): " + text.substring(6));
+        }
+
+}
